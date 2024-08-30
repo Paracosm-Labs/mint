5ab39be5de969238db21ec879fd06aac1666f685
mypttwo <mypt.two@gmail.com>
2024-08-30 23:54:12 +0530

    moving login related code

--- app/header.js
+++ app/header.js
@@ -5,69 +5,21 @@ import {useAuth} from "@/lib/AuthContext";
 import { getAddress, signUsingWallet, verifyWallet } from '@/lib/wallet';
 import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/navigation';
+import login from "@/lib/login";
 
 function Header() {
   const { isAuthenticated } = useAuth();
   const { setIsAuthenticated, setJwtToken } = useAuth();
   const router = useRouter();
 
-  const verifySignature = async (signature, address) =>{
-    const response = await fetch('/api/auth/verify', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ sign: signature, address : address }),
-    });
-    if(response.status == 200){
-      const resJson = await response.json();
-      return resJson.auth;
-    }
-    return null;
-  }
-
-  const requestNonce = async (address_) => {
-      const response = await fetch('/api/auth/req', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ address: address_ }),
-      });
-      const resJson = await response.json();
-      return resJson.nonce;
+  const postLogin = (auth) => {
+    setIsAuthenticated(true);
+    setJwtToken(auth);
+    router.push('/dashboard/business', { scroll: false })    
   }
 
   const handleLoginClick = () => {
-    verifyWallet().then(res => {
-      if(!res || res.length == 0){
-        alert("Please install or login to Tronlink to proceed.");
-        return;
-      }
-      if(res.code != 200){
-        alert(res.message);
-        return;
-      }
-      getAddress().then(address => {
-        if(address){
-          requestNonce(address).then(nonce => {
-            if(nonce){
-              signUsingWallet(nonce).then(signature => {
-                verifySignature(signature, address).then((auth) => {
-                  setIsAuthenticated(true);
-                  setJwtToken(auth);
-                  router.push('/dashboard/business', { scroll: false })
-                });
-              });
-              
-            } else throw new Error(("Cannot get nonce"));
-          });
-        } else throw new Error(("Cannot get address"));
-        
-      }); 
-    }).catch(err =>{
-      console.error(err);
-    })
+    login(postLogin)
   } 
 
   const handleLogoutClick = () => {
--- app/login/page.js
+++ app/login/page.js
@@ -1,14 +1,27 @@
 'use client'
 import React from 'react';
+import { useRouter } from 'next/navigation';
+import {useAuth} from "@/lib/AuthContext";
+import login from "@/lib/login";
 
 const Login = () => {
-
+    const { setIsAuthenticated, setJwtToken } = useAuth();
+    const router = useRouter();
+  
+    const postLogin = (auth) => {
+      setIsAuthenticated(true);
+      setJwtToken(auth);
+      router.push('/dashboard/business', { scroll: false })    
+    }
+    const handleLoginClick = () => {
+        login(postLogin)
+      } 
     return(
         <main class="d-flex align-items-center py-4 form-signin w-100 m-auto">
             <div>
                 <p class="h3 my-3 fw-normal">Welcome to MintDeals</p>
                 <p class="mb-3 text-body-secondary">Remember to Login to Tronlink before proceeding!</p>
-                <button class="btn btn-primary w-100 py-2" type="submit">Log in</button>
+                <button class="btn btn-primary w-100 py-2" type="button" onClick={handleLoginClick}>Log in</button>
             </div>
         </main>        
     )
